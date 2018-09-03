4db8278832614c45e8544e2ca49e31078ae11002
Ichiro Arai <arai_ichiro@cyberagent.co.jp>
2018-09-03 17:20:25 +0900

    add compound benchmark test

--- README.md
+++ README.md
@@ -13,20 +13,24 @@ $ curl http://localhost:8080/heavy
 OK!
 $ curl http://localhost:8080/envoy
 OK!
+$ curl http://localhost:8080/light
+ok
 ```
 
 ## Gatling
 
+### Single Request Scenario
+
 Gatling test without circuit breaker:
 
 ```
-$ QPS=400 REQUEST=/heavy sbt "benchmark/gatling:testOnly AppRequestSimulation"
+$ QPS=400 REQUEST=/heavy sbt "benchmark/gatling:testOnly SingleRequestSimulation"
 ```
 
 Gatling test with circuit breaker:
 
 ```
-$ QPS=400 REQUEST=/envoy sbt "benchmark/gatling:testOnly AppRequestSimulation"
+$ QPS=400 REQUEST=/envoy sbt "benchmark/gatling:testOnly SingleRequestSimulation"
 ```
 
 Open result:
@@ -34,6 +38,27 @@ Open result:
 $ open ./benchmark/target/gatling/*/index.html
 ```
 
+### Compound Request Scenario
+
+Gatling test without circuit breaker:
+
+```
+$ QPS=400 REQUEST_1=/heavy REQUEST_2=/light sbt "benchmark/gatling:testOnly CompoundRequestSimulation"
+```
+
+Gatling test with circuit breaker:
+
+```
+$ QPS=400 REQUEST_1=/envoy REQUEST_2=/light sbt "benchmark/gatling:testOnly CompoundRequestSimulation"
+```
+
+Open result:
+
+```
+$ open ./benchmark/target/gatling/*/index.html
+```
+
+
 ## Layout
 
-![Layout](https://github.com/ichiro-arai/envoy-circuit-breaking-example/blob/images/layout.svg "layout")
+![Layout](https://github.com/ichiro-arai/envoy-circuit-breaking-example/blob/images/layout-compound.svg "layout")
--- benchmark/src/test/scala/CompoundRequestSimulation.scala
+++ benchmark/src/test/scala/CompoundRequestSimulation.scala
@@ -4,29 +4,43 @@ import io.gatling.http.Predef.{http, status}
 
 import scala.concurrent.duration._
 
-class AppRequestSimulation extends Simulation {
+class CompoundRequestSimulation extends Simulation {
   val qps: Int = sys.env.getOrElse("QPS", "100").toInt
   val duration: FiniteDuration = sys.env.getOrElse("DURATION", "1").toInt.minutes
 
-  val scn: ScenarioBuilder = {
+  val scn1: ScenarioBuilder = {
     val server = sys.env.getOrElse("URL", "http://localhost:8080")
-    val request = sys.env.getOrElse("REQUEST", "/")
+    val request = sys.env.getOrElse("REQUEST_1", "/envoy")
 
-    scenario("AppRequestSimulation").exec(
-      http("app-request")
+    scenario("CompoundRequestSimulation1").exec(
+      http("app-request-1")
+        .get(s"$server$request")
+        .check(status.is(200))
+    )
+  }
+  val scn2: ScenarioBuilder = {
+    val server = sys.env.getOrElse("URL", "http://localhost:8080")
+    val request = sys.env.getOrElse("REQUEST_2", "/local")
+
+    scenario("CompoundRequestSimulation2").exec(
+      http("app-request-2")
         .get(s"$server$request")
         .check(status.is(200))
     )
   }
 
   setUp(
-    scn
+    scn1
       .inject(
         rampUsersPerSec(qps / 4) to qps during (duration / 4),
         constantUsersPerSec(qps) during (duration / 4),
         rampUsersPerSec(qps) to (qps * 1.5) during (duration / 4),
         constantUsersPerSec(qps) during (duration / 4),
-      )
+      ),
+    scn2
+      .inject(
+        constantUsersPerSec(qps) during duration,
+      ),
   ).protocols(
     http
       .connectionHeader("keep-alive")
